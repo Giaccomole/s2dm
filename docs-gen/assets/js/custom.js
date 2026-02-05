f72028454b18962eb5fa5b5f2cf5eb2acd18eb20
Giaccomole <orhunucak@gmail.com>
2026-02-05 13:50:01 +0100

    fix: improve search index URL handling for GitHub Pages
    Signed-off-by: Giaccomole <orhunucak@gmail.com>

--- docs-gen/assets/js/custom.js
+++ docs-gen/assets/js/custom.js
@@ -3,13 +3,27 @@
 // Fix search index URL for GitHub Pages
 (function() {
   // Override fetch to fix search-index.json URL
-  const originalFetch = window.fetch;
+  const originalFetch = window.fetch.bind(window);
   window.fetch = function(url, options) {
     if (typeof url === 'string' && url === '/search-index.json') {
-      // Get the base URL from the current location
-      const baseUrl = window.location.pathname.split('/').slice(0, -1).join('/');
-      url = baseUrl + '/search-index.json';
+      const origin = window.location.origin;
+      const primaryUrl = origin + '/search-index.json';
+      const pathSegments = window.location.pathname.split('/').filter(Boolean);
+      const baseSegment = pathSegments.length > 0 ? pathSegments[0] : '';
+      const fallbackUrl = baseSegment
+        ? origin + '/' + baseSegment + '/search-index.json'
+        : primaryUrl;
+
+      return originalFetch(primaryUrl, options).then((response) => {
+        if (response && response.ok) {
+          return response;
+        }
+        if (fallbackUrl === primaryUrl) {
+          return response;
+        }
+        return originalFetch(fallbackUrl, options);
+      });
     }
-    return originalFetch.call(this, url, options);
+    return originalFetch(url, options);
   };
 })();
